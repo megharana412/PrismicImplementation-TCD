d975ff73ed1120d5a489be876b03b2c0c7e87132
megha0412 <megha04.12.96@gmail.com>
2022-07-28 16:14:23 +0530

    Preview Draft

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,10 +1,49 @@
 class AboutController < ApplicationController
-
   def index
-    response = api.query(
-      Prismic::Predicates.at("document.type", "about")
-      )
-    @documents = response.results
-    render '/about/index'
+    api = Prismic.api('https://tcd-project2.prismic.io/api/v1')
+    
+    response = api.all({
+        "page" => params[:page] ? params[:page] : "1",
+        "page_size" => params[:page_size] ? params[:page_size] : "20",
+        "ref" => ref})
+    @documents = response.results[0]
   end
+
+  # Single-document page action: mostly, setting the @document instance variable, and checking the URL
+  def document
+    id = params[:id]
+    slug = params[:slug]
+
+    @google_id = api.experiments.current
+    @document = api.getByID(id, {"ref" => ref})
+
+    # This is how an URL gets checked (with a clean redirect if the slug is one that used to be right, but has changed)
+    # Of course, you can change slug_checker in prismic_service.rb, depending on your URL strategy.
+    @slug_checker = PrismicService.slug_checker(@document, slug)
+    if !@slug_checker[:correct]
+      render inline: "Document not found", status: :not_found if !@slug_checker[:redirect]
+      redirect_to document_path(id, @document.slug), status: :moved_permanently if @slug_checker[:redirect]
+    end
+  end
+
+  # Search result: querying all documents containing the q parameter
+  def search
+    @google_id = api.experiments.current
+    @documents = api.form('everything')
+                    .query(Prismic::Predicates.fulltext('document', params[:q]))
+                    .page(params[:page] ? params[:page] : '1')
+                    .page_size(params[:page_size] ? params[:page_size] : '20')
+                    .submit(ref)
+  end
+
+
+  # For writers to preview a draft with the real layout
+  def preview
+    api = Prismic.api('https://tcd-project2.prismic.io/api/v1')
+    preview_token = params[:token]
+    redirect_url = api.preview_session(preview_token, link_resolver(), '/')
+    cookies[Prismic::PREVIEW_COOKIE] = { value: preview_token, expires: 30.minutes.from_now }
+    redirect_to redirect_url
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,41 @@
 class ApplicationController < ActionController::API
+  # If something goes wrong, it could be because of an invalid preview token
+  def clearcookies
+    cookies.delete Prismic::PREVIEW_COOKIE
+    redirect_to '/'
+  end
 
-    def index
-      document = api.getByUID("about", "tcd_about_page", { "ref" => ref })
+  def api
+    @api = Prismic.api('https://tcd-project2.prismic.io/api/v1')
+  end
+
+  # Setting @ref as the actual ref id being queried, even if it's the master ref.
+  # To be used to call the API, for instance: api.form('everything').submit(ref)
+  def ref
+    @ref ||= preview_ref || experiment_ref || api.master_ref.ref
+  end
+
+  def preview_ref
+    if request.cookies.has_key?(Prismic::PREVIEW_COOKIE)
+      request.cookies[Prismic::PREVIEW_COOKIE]
+    else
+      nil
     end
+  end
 
-    def api
-        @api = Prismic.api('https://tcd-project2.prismic.io/api/v1')
-      end
-      
-      def ref
-        @ref ||= preview_ref || experiment_ref || api.master_ref.ref
-      end
-      
-      def preview_ref
-        if request.cookies.has_key?(Prismic::PREVIEW_COOKIE)
-          request.cookies[Prismic::PREVIEW_COOKIE]
-        else
-          nil
-        end
-      end
-      
-      def experiment_ref
-        if request.cookies.has_key?(Prismic::EXPERIMENTS_COOKIE)
-          api.experiments.ref_from_cookie(request.cookies[Prismic::EXPERIMENTS_COOKIE])
-        else
-          nil
-        end
-      end
+  def experiment_ref
+    if request.cookies.has_key?(Prismic::EXPERIMENTS_COOKIE)
+      api.experiments.ref_from_cookie(request.cookies[Prismic::EXPERIMENTS_COOKIE])
+    else
+      nil
+    end
+  end
+
+  ##
+
+  # Easier access and initialization of the Prismic::API object.
+  def api
+    @api ||= PrismicService.init_api
+  end
+
 end
